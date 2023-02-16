0eaf14a2cc239836915b807f8399cfd1629a16d9
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-02-16 23:44:35 +0500

    Update NavBar.tsx
    ml-auto mr-auto

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,15 +10,15 @@ export default function NavBar() {
     <>
        <div className="border px-2"> 
        
-          <div className="flex justify-center border">
-          <img src="./panaverse80_80.jpg" className="h-12 w-12 p-2  mr-auto"/>
-          <a className="m-4 ">Home</a>
-          <a className="m-4">Docs</a> 
-          <a className="m-4">Future</a>
-          <a className="m-4">More</a>
+          <div className="flex flex-col sm:flex-row justify-center border">
+          <img src="./panaverse80_80.jpg" className="h-12 w-12 p-2 ml-auto  mr-auto"/>
+          <a className="m-4 ml-auto  mr-auto ">Home</a>
+          <a className="m-4 ml-auto  mr-auto">Docs</a> 
+          <a className="m-4 ml-auto  mr-auto">Future</a>
+          <a className="m-4 ml-auto  mr-auto">More</a>
           
           
-          <button className="text-white bg-red-400 hover:bg-red-500  ml-auto my-2 p-2 rounded-full" >
+          <button className="text-white bg-red-400 hover:bg-red-500  ml-auto mr-auto my-2 p-2 rounded-full" >
           <a href={'https://portal.piaic.org/signup'}>Apply Now</a>
             </button>
             
