403b200d487c1d4fe1aaf458ecefdd1af19de86e
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-02-17 12:02:05 +0500

    Update Main_Section.tsx

--- app/components/Main_Section.tsx
+++ app/components/Main_Section.tsx
@@ -2,19 +2,19 @@
     return (
       <div className="p-16" >
         <div className="flex flex-col justify-center">
-        <p className="flex justify-center bg-clip-text text-transparent py-4
+        <p className="flex text-left sm:justify-center text-base sm:text-xl md:text-3xl bg-clip-text text-transparent py-4
               sm:font-thin md:font-normal lg:font-bold
-              bg-gradient-to-l from-gray-50 to-black gray text-3xl">
+              bg-gradient-to-l from-gray-50 to-black gray ">
               Certified Web 3.0 and Metaverse Developer
               </p>
           
-            <p className="flex justify-center py-4 text-red-400 sm:font-thin md:font-normal lg:font-medium text-4xl">
+            <p className="flex text-left sm:justify-center  text-base sm:text-xl md:text-2xl lg:text-3xl py-4 text-red-400 sm:font-thin md:font-normal lg:font-medium ">
             A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar
             </p>
          
-          <p className="flex justify-center py-2 text-gray-400 text-2xl">
+          <p className="flex text-left sm:justify-center py-2 text-gray-400 text-base sm:text-xl md:text-2xl">
           Getting Ready for the Next Generation and Future of the Internet</p>
-          <p className="flex justify-center py-2 text-gray-400 text-2xl">
+          <p className="flex text-left sm:justify-center py-2 text-gray-400 text-base sm:text-xl md:text-2xl">
           Join a 13 Trillion Dollar Industry with 5 Billion Users</p>
           <div className="p-6 flex flex-col sm:flex-row justify-center">
             <button className="animate-bounce text-white rounded-full px-4 m-2 py-2 bg-red-400 hover:bg-red-500">
