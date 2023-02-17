5ce34fdc32342219e287c70286523979d9927a0b
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-02-17 16:26:18 +0500

    mobile navbar updated

--- app/components/Main_Section.tsx
+++ app/components/Main_Section.tsx
@@ -2,13 +2,18 @@
     return (
       <div className="p-16" >
         <div className="flex flex-col justify-center">
-        <p className="flex text-left sm:justify-center text-base sm:text-xl md:text-3xl bg-clip-text text-transparent py-4
+              <p className="flex text-left  sm:collapse sm:justify-center text-base font-bold sm:text-xl md:text-3xl bg-clip-text text-transparent py-4
+              sm:font-thin md:font-normal lg:font-bold
+              text-black animate-pulse ">
+              Certified Web 3.0 and Metaverse Developer
+              </p>
+              <p className="flex text-left collapse sm:visible sm:justify-center text-base sm:text-xl md:text-3xl bg-clip-text text-transparent py-4
               sm:font-thin md:font-normal lg:font-bold
               bg-gradient-to-l from-gray-50 to-black gray animate-pulse ">
               Certified Web 3.0 and Metaverse Developer
               </p>
           
-            <p className="flex text-left sm:collapse sm:justify-center  text-base sm:text-xl md:text-2xl lg:text-3xl py-4 text-red-400 sm:font-thin md:font-normal lg:font-medium ">
+            <p className="flex text-left sm:collapse sm:justify-center font-bold sm:text-xl md:text-2xl lg:text-3xl py-4 text-red-400 sm:font-thin md:font-normal lg:font-medium ">
             In Karachi, Lahore, Islamabad, and Peshawar
             </p>
             <p className="flex text-left collapse sm:visible sm:justify-center  text-base sm:text-xl md:text-2xl lg:text-3xl py-4 text-red-400 sm:font-thin md:font-normal lg:font-medium ">
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,16 +10,18 @@ export default function NavBar() {
     <>
        <div className="border px-2"> 
        
-          <div className="flex flex-col sm:flex-row justify-center border">
-          <img src="./panaverse80_80.jpg" className="h-12 w-12 p-2  mr-auto"/>
+          <div className="flex sm:flex-row justify-center border">
+          <img src="./panaverse80_80.jpg" className="h-12 w-12 p-2 mr-auto"/>
+          <div className="flex flex-col sm:flex-row">
+          <a className="m-4 ">Home</a>
+          <a className="m-4 ">Docs</a> 
+          </div>
+          <div className="flex flex-col sm:flex-row">
+          <a className="m-4 ">Future</a>
+          <a className="m-4 ">More</a>
+          </div>
           
-          <a className="m-4 mr-auto ml-auto ">Home</a>
-          <a className="m-4 mr-auto ml-auto  ">Docs</a> 
-          <a className="m-4 mr-auto ml-auto ">Future</a>
-          <a className="m-4 mr-auto ml-auto ">More</a>
-          
-          
-          <button className="text-white collapse sm:visible bg-red-400 hover:bg-red-500  ml-auto mr-auto my-2 p-2 rounded-full" >
+          <button className="text-white collapse sm:visible bg-red-400 hover:bg-red-500 ml-auto my-2 p-2 rounded-full" >
           <a href={'https://portal.piaic.org/signup'}>Apply Now</a>
             </button>
             
