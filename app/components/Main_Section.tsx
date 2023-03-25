529eae3c996cd1ca2d4002dbd7f9a06052ace578
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-03-25 19:13:56 +0500

    AOS added

--- app/components/Main_Section.tsx
+++ app/components/Main_Section.tsx
@@ -1,24 +1,29 @@
+  "use client"
+  import { useEffect } from "react";
+  import AOS from 'aos'
+  import 'aos/dist/aos.css';
   export default function Main_Section() {
+    useEffect(()=>{AOS.init();},[])
     return (
-      <div className="h-screen" >
-        <span className="h-4/5 flex flex-col justify-around items-center">
+      <div className="h-screen shadow-2xl" >
+        <span className="h-4/5 flex flex-col justify-around items-center ">
             
               <span className="  text-xl sm:text-2xl font-bold text-center
-              bg-clip-text text-transparent bg-gradient-to-l from-gray-400 to-black">
+              bg-clip-text text-transparent bg-gradient-to-l from-gray-400 to-black" data-aos="fade-right" >
               Certified Web 3.0 and Metaverse Developer
               </span>
           
-            <span className="font-bold text-base text-center sm:text-xl md:text-2xl lg:text-3xl text-red-400 sm:font-bold md:font-bold lg:font-bold ">
+            <span className="font-bold text-base text-center sm:text-xl md:text-2xl lg:text-3xl text-red-400 sm:font-bold md:font-bold lg:font-bold" data-aos="fade-right" >
             A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar
             </span>
             
-           <span className="flex flex-col justify-between items-center"> 
-          <span className="font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl">
+           <span className="flex flex-col justify-between items-center " > 
+          <span className="font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
           Getting Ready for the Next Generation and Future of the Internet</span>
-          <span className="visible font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl">
+          <span className="visible font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
           Join a 13 Trillion Dollar Industry with 5 Billion Users</span>
           </span>
-          <span className="flex flex-col sm:flex-row">
+          <span className="flex flex-col sm:flex-row" data-aos="fade-right">
             <button className="mx-2 px-2 animate-bounce  text-white h-10 rounded-full bg-red-400 hover:bg-red-500">
              <a href={'https://portal.piaic.org/signup'}>Apply Now</a>
             </button>
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,16 +10,17 @@ export default function NavBar() {
     <>
        <div className=""> 
        
-          <div className="flex sm:flex-row border justify-between">
+          <div className="flex flex-wrap border justify-between items-center">
           <img src="./panaverse80_80.jpg" className="h-12 w-12 p-2"/>
-          <div className="flex flex-row flex-wrap">
+          {/*  */}
+          <ul className="flex flex-wrap order-last sm:order-none sm:shrink-0 ">
           <a className="m-4 " href="/">Home</a>
           <a className="m-4 " href="/metaverse">Metaverse</a> 
           <a className="m-4 " href="/community">Community</a>
           <a className="m-4 ">More</a>
-          </div>
-          <div className="flex">
-          <button className="flex-shrink-0 flex-grow-0 h-10 text-white sm:visible bg-red-400 hover:bg-red-500  my-2 p-2 rounded-full" >
+          </ul>
+          <div className="flex order-none sm:order-last ">
+          <button className="flex-grow flex-shrink-0 h-10 text-white sm:visible bg-red-400 hover:bg-red-500  my-2 p-2 rounded-full" >
           <a href={'https://portal.piaic.org/signup'}>Apply Now</a>
           </button>
           </div>
