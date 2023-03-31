3ac15fc9e60cf8df4887c0766add9367d25540d3
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-03-31 15:12:30 +0500

    Update Main_Section.tsx

--- app/components/Main_Section.tsx
+++ app/components/Main_Section.tsx
@@ -5,11 +5,11 @@
   export default function Main_Section() {
     useEffect(()=>{AOS.init();},[])
     return (
-      <div className="h-screen shadow-2xl" >
+      <div className="h-screen shadow-2xl  bg-gradient-to-bl from-blue-100 to-blue-900 " >
         <span className="h-4/5 flex flex-col justify-around items-center ">
             
               <span className="  text-xl sm:text-2xl font-bold text-center
-              bg-clip-text text-transparent bg-gradient-to-l from-gray-400 to-black" data-aos="fade-right" >
+              bg-clip-text text-transparent bg-gradient-to-l from-gray-900 to-black" data-aos="fade-right" >
               Certified Web 3.0 and Metaverse Developer
               </span>
           
@@ -18,9 +18,9 @@
             </span>
             
            <span className="flex flex-col justify-between items-center " > 
-          <span className="font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
+          <span className="font-bold text-center sm:justify-center text-gray-900 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
           Getting Ready for the Next Generation and Future of the Internet</span>
-          <span className="visible font-bold text-center sm:justify-center text-gray-400 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
+          <span className="visible font-bold text-center sm:justify-center text-gray-900 text-sm sm:text-xl md:text-2xl" data-aos="fade-right">
           Join a 13 Trillion Dollar Industry with 5 Billion Users</span>
           </span>
           <span className="flex flex-col sm:flex-row" data-aos="fade-right">
