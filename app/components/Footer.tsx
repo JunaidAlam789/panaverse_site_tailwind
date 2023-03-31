e4b2ccebf26196438d6b31d7689b3aaa41212fc6
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-03-31 22:20:33 +0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -19,24 +19,24 @@ export default function Footer() {
         <div className="flex flex-col sm:flex-row justify-center">
         <div className="pr-20 pt-10 pl-2">
         <Logo className="h-16 w-16 p-2" />
-        <p className="pl-2"> Panaverse DAO </p>
+        <p className="pl-2 text-white"> Panaverse DAO </p>
         </div>
        
           
-          <div className="flex flex-col p-4">
-            <p className="p-2 pb-3 font-medium" >Company</p>
+          <div className="flex flex-col p-4 text-white font-bold">
+            <p className="p-2 pb-3 " >Company</p>
             <a className="p-2" href={'#'}>Panverse DAO</a>
             <a className="p-2" href={'#'}>Panaverse Community</a>
             
           </div>
-          <div className="flex flex-col p-4">
-            <p className="p-2 pb-3 pr-2 font-medium">About Us</p>
+          <div className="flex flex-col p-4 text-white font-bold">
+            <p className="p-2 pb-3 pr-2 ">About Us</p>
             <a className="p-2 pr-2" href={'#'}>Faculty</a>
             <a className="p-2 pr-2"href={'#'}>Locations</a>
             
           </div>
-           <div className="flex flex-col p-4">
-            <p className="p-2 pb-3 font-medium">Follow Us</p>
+           <div className="flex flex-col p-4 text-white font-bold">
+            <p className="p-2 pb-3 ">Follow Us</p>
             <a className="p-2" href={'#'}>Facebook</a>
             <a className="p-2" href={'#'}>Twitter</a>
            </div>
